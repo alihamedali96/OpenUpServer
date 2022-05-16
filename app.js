fdf20def7d7a31af16752a9c046f810b7c1a2297
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-05-16 14:33:36 +0100

    change data files to .json and change routes to match json format

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const allPosts = require('./allPosts')
-const myPosts = require('./myPosts')
+// const allPosts = require('./allPosts')
+// const myPosts = require('./myPosts')
+const fs = require('fs')
+
+
+function getAllPosts() {
+    try {
+    const jsonString = fs.readFileSync('./allPost.json', 'utf-8')
+    const allPosts = JSON.parse(jsonString)
+    return allPosts
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+function findTopPosts() {
+  try {
+    const jsonString = fs.readFileSync('./allPost.json', 'utf-8')
+    const allPosts = JSON.parse(jsonString)
+    let sortedPosts = allPosts.sort((a, b) => (a.interactions < b.interactions) ? 1 : -1)
+    return sortedPosts.slice(0, 2)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+
+
+// jsonReader('./allPosts.json', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+//update json
+
+
 
 app.use(express.json());
 app.use(cors()) ;
-
-
-function findTopPosts() {
-  let sortedPosts = allPosts.sort((a, b) => (a.interactions < b.interactions) ? 1 : -1)
-  return sortedPosts.slice(0, 2)
-}
 
 
 app.get('/', (req, res) => { 
@@ -19,11 +51,11 @@ app.get('/', (req, res) => {
   })
 
 app.get('/homepage', (req, res) => {
-  res.send(findTopPosts(allPosts))
+  res.send(findTopPosts())
 })
 
 app.get('/allposts', (req, res) => {
-  res.send(allPosts)
+  res.send(getAllPosts())
 })
 
 app.get('/myjournal', (req, res) => {
