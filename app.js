c90b34e288f7f9bcf7906297dd47bb7b76c389e0
Ali Hamed Ali <59931761+alihamedali96@users.noreply.github.com>
2022-05-16 17:57:00 +0400

    Install heroku

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
 app.use(express.json());
 app.use(cors()) ;
 
 app.get('/', (req, res) => { 
     res.send('Welcome to our Open Up API!'); 
   })
+
+  module.exports = app;
